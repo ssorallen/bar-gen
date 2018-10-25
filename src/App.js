a119619607b2eb9ab9f6ff91e90dd5b27db15b21
Ross Allen <rallen@boweryfarming.com>
2018-10-25 14:53:13 -0400

    Add CASE and radio-like btns

--- src/App.js
+++ src/App.js
@@ -41,8 +41,11 @@ class App extends Component<Props, State> {
   }
 
   setActiveBtn(nextActiveBtn: string) {
+    // Choosing a new type should also reset the number because there's little chance you want the
+    // same number for that other type.
     this.setState({
       activeBtn: nextActiveBtn,
+      activeNumber: '',
     });
   }
 
@@ -52,9 +55,11 @@ class App extends Component<Props, State> {
     });
   }
 
-  handleResetClick = () => {
+  handleDeleteClick = () => {
+    // Can't delete if there's no number.
+    if (this.state.activeNumber.length === 0) return;
     this.setState({
-      activeNumber: '',
+      activeNumber: this.state.activeNumber.substr(0, this.state.activeNumber.length - 1),
     });
   };
 
@@ -95,35 +100,112 @@ class App extends Component<Props, State> {
             <span className="cursor" />
           </pre>
         </h4>
-        <div className="btn-group" role="group">
-          <button
-            className="btn btn-danger px-4"
-            disabled={this.state.activeBtn === 'BIN'}
-            onClick={this.setActiveBtn.bind(this, 'BIN')}
-            type="button">
-            BIN
-          </button>
-          <button
-            className="btn btn-primary px-3"
-            disabled={this.state.activeBtn === 'TRAY'}
-            onClick={this.setActiveBtn.bind(this, 'TRAY')}
-            type="button">
-            TRAY
-          </button>
-          <button
-            className="btn btn-success"
-            disabled={this.state.activeBtn === 'STATION'}
-            onClick={this.setActiveBtn.bind(this, 'STATION')}
-            type="button">
-            STATION
-          </button>
-          <button
-            className="btn btn-warning"
-            disabled={this.state.activeBtn === 'CROP'}
-            onClick={this.setActiveBtn.bind(this, 'CROP')}
-            type="button">
-            CROP
-          </button>
+        <div className="row">
+          <div className="col my-2">
+            <button
+              className="btn btn-rounded btn-danger px-4"
+              disabled={this.state.activeBtn === 'BIN'}
+              onClick={this.setActiveBtn.bind(this, 'BIN')}
+              type="button">
+              <div className="form-check">
+                <input
+                  checked={this.state.activeBtn === 'BIN'}
+                  className="form-check-input"
+                  id="resource-type-bin"
+                  name="resource-type"
+                  readOnly
+                  type="radio"
+                />
+                <label className="form-check-label" htmlFor="resource-type-bin">
+                  BIN
+                </label>
+              </div>
+            </button>
+          </div>
+          <div className="col my-2">
+            <button
+              className="btn btn-rounded btn-secondary px-3"
+              disabled={this.state.activeBtn === 'CASE'}
+              onClick={this.setActiveBtn.bind(this, 'CASE')}
+              type="button">
+              <div className="form-check">
+                <input
+                  checked={this.state.activeBtn === 'CASE'}
+                  className="form-check-input"
+                  id="resource-type-case"
+                  name="resource-type"
+                  readOnly
+                  type="radio"
+                />
+                <label className="form-check-label" htmlFor="resource-type-case">
+                  CASE
+                </label>
+              </div>
+            </button>
+          </div>
+          <div className="col my-2">
+            <button
+              className="btn btn-rounded btn-primary px-3"
+              disabled={this.state.activeBtn === 'TRAY'}
+              onClick={this.setActiveBtn.bind(this, 'TRAY')}
+              type="button">
+              <div className="form-check">
+                <input
+                  checked={this.state.activeBtn === 'TRAY'}
+                  className="form-check-input"
+                  id="resource-type-tray"
+                  name="resource-type"
+                  readOnly
+                  type="radio"
+                />
+                <label className="form-check-label" htmlFor="resource-type-tray">
+                  TRAY
+                </label>
+              </div>
+            </button>
+          </div>
+          <div className="col my-2">
+            <button
+              className="btn btn-rounded btn-success"
+              disabled={this.state.activeBtn === 'STATION'}
+              onClick={this.setActiveBtn.bind(this, 'STATION')}
+              type="button">
+              <div className="form-check">
+                <input
+                  checked={this.state.activeBtn === 'STATION'}
+                  className="form-check-input"
+                  id="resource-type-station"
+                  name="resource-type"
+                  readOnly
+                  type="radio"
+                />
+                <label className="form-check-label" htmlFor="resource-type-station">
+                  STATION
+                </label>
+              </div>
+            </button>
+          </div>
+          <div className="col my-2">
+            <button
+              className="btn btn-rounded btn-warning"
+              disabled={this.state.activeBtn === 'CROP'}
+              onClick={this.setActiveBtn.bind(this, 'CROP')}
+              type="button">
+              <div className="form-check">
+                <input
+                  checked={this.state.activeBtn === 'CROP'}
+                  className="form-check-input"
+                  id="resource-type-crop"
+                  name="resource-type"
+                  readOnly
+                  type="radio"
+                />
+                <label className="form-check-label" htmlFor="resource-type-crop">
+                  CROP
+                </label>
+              </div>
+            </button>
+          </div>
         </div>
         <div className="mt-4 mx-auto" style={{ width: '240px' }}>
           <div className="row">
@@ -208,7 +290,7 @@ class App extends Component<Props, State> {
               <button
                 className="btn btn-dark btn-round btn-lg"
                 disabled={this.state.activeNumber === ''}
-                onClick={this.handleResetClick}
+                onClick={this.handleDeleteClick}
                 style={{ textIndent: '-3px' }}>
                 ⌫
               </button>
