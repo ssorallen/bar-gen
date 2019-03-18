b513c53722a8d863fd08fa1c647ddeb168823e11
Ross Allen <rallen@boweryfarming.com>
2019-03-18 08:30:52 -0400

    Updates

---
+++ precache-manifest.cc7384ac94dfa94f09400051780fd92c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcd512741949d8d6d5c4",
+    "url": "/bar-gen/static/css/main.5751c319.chunk.css"
+  },
+  {
+    "revision": "dcd512741949d8d6d5c4",
+    "url": "/bar-gen/static/js/main.c5017728.chunk.js"
+  },
+  {
+    "revision": "03f8549df6f3a4301f67",
+    "url": "/bar-gen/static/js/runtime~main.83fb5063.js"
+  },
+  {
+    "revision": "3d328cffaf783d9ffaac",
+    "url": "/bar-gen/static/css/2.55c3e497.chunk.css"
+  },
+  {
+    "revision": "3d328cffaf783d9ffaac",
+    "url": "/bar-gen/static/js/2.d3e66738.chunk.js"
+  },
+  {
+    "revision": "618250dd6e2a9a441e2c1c092cbd3e15",
+    "url": "/bar-gen/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bar-gen/precache-manifest.6407dfc8d9fc45b0719ce6bb53aed552.js"
+  "/bar-gen/precache-manifest.cc7384ac94dfa94f09400051780fd92c.js"
 );
 
 workbox.clientsClaim();
